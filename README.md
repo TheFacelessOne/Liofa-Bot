01fd94298b60f6593abcbe161fcb494088a2831a
TheFacelessOne <wiichris@hotmail.com>
2021-07-19 17:06:53 +0100

    1.0.0 created
    Added version 1.0.0
    
    Many things to improve upon but seems like a good start
    
    Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # Liofa Bot
  A Discord Bot for dealing with people speaking other languages
+
+ ## Features
+- Instructs user to speak English in whichever language they're speaking (If known)
+- Deletes  all future non-English Messages if User sends 3 messages within 30 minutes that are not English
+
+## Commands
+Input | Function
+------------|--------------------
+`**enable` | Turns on Liofa  
+`**disable` | Turns off Liofa
+
+## Translations
+I would appreciate any and all help with translations. I only speak English myself so most translations I am using are from google translate or my friends.
+
+## To Do List
+- Add in configurable settings per server for 
+   - [ ] Time
+   - [ ] Warning count
+   - [ ] Acceptable Languages
+- Add commands to help with moderation
+    - [ ] Remove warnings
+    - [ ] Reset Timer
+    - [ ] Remove User
+- [ ] Add configurable roles that are excluded
+- [ ] Add a way for moderators to tell what language someone is speaking
+- [ ] Learn how to use databases so I'm not saving information in a JSON
+
+##### About Liofa
+Liofa (or Líofa) is Irish for "fluent".  
+I came up with the idea because I'm an administrator for a discord server where it is against the rules to speak languages other than English. I found it frustrating to have to translate "Please speak English" into so many languages that I decided to make a command using another Discord bot to do this instead. Then I had the idea to just automate the process with my own bot.  
+I hope you all enjoy my little bot. If you have any questions or suggestions, please do let me know. I'm still relatively new to coding so I accept any and all criticism.
---
+++ bot.js
@@ -0,0 +1,120 @@
+// Defines required stuff
+require("dotenv").config();
+const cld = require('cld');
+const Discord = require("discord.js");
+const fs = require("fs");
+
+// Discord stuff
+const client = new Discord.Client();
+client.login(process.env.BOTTOKEN);
+
+// Variables
+const LiofaMessages = require("./Server Data/Responses");
+const RawWatch = fs.readFileSync("./Server Data/WatchList.json");
+const LiofaWatch = JSON.parse(RawWatch);
+let LiofaState = true;
+
+//Defines callbacks
+client.on("message", messageRec);
+
+// Start-up confirmation
+client.once('ready', () => {
+	console.log('Líofa is Talking!');
+});
+
+
+
+// Run on every message
+async function messageRec(msg) {
+
+  // Checks if Liofa should run on this message
+  if (runLiofa(msg) === false) {
+    return;
+  }
+
+  // Runs Liofa
+  try {
+    // Asks what the language is
+    const result = await liofaCheck(msg.content);
+
+
+    // Gives output if it's not English
+    if (result.code != "en") {
+
+      //Warnings Check
+      let warnCount = liofaMod(msg.guild.id, msg.author.id);
+      if (warnCount < 3) {
+
+        // Checks if output for given language is available
+        if (typeof LiofaMessages[result.code] === 'string') {
+          msg.reply(LiofaMessages[result.code]);
+        } else {
+          msg.reply("Please Speak English");
+          msg.channel.send(result.name + " must be added to Languages");
+        }
+      } else if (warnCount == 3) {
+        msg.reply("All further messages will be deleted unless you talk English");
+      } else if (warnCount > 3) {
+        msg.delete();
+      }
+    }
+  }
+
+
+  // Returns error for when language cannot be detected 
+  catch {
+    return;
+  }
+}
+
+// Checks if Liofa should run
+function runLiofa(msg2) {
+  // Checks if it's a Bot
+  if (msg2.author.bot === true) {
+    return false;
+  
+  // Checks if Liofa is turned on  
+  } else if (msg2.content.includes("**")) {
+    if (msg2.content === "**enable") {
+      LiofaState = true;
+      msg2.reply("Liofa is enabled")
+    }else if(msg2.content === "**disable") {
+      LiofaState = false;
+      msg2.reply("Liofa is disabled")
+    }
+  }
+  return LiofaState;
+  // TODO Add excluded roles in here
+}
+
+
+// Check for Language
+async function liofaCheck(LiofaMsg) {
+  const LiofaResult = await cld.detect(LiofaMsg);
+  return LiofaResult.languages[0];
+}
+
+// Check Warning Status
+function liofaMod(ServerID, UserID) {
+  if (typeof LiofaWatch[ServerID][UserID].warnings === 'undefined') { 
+    LiofaWatch[ServerID][UserID] = {warnings : 1, time : Date.now()};
+
+  } else if ((Date.now() - LiofaWatch[ServerID][UserID].time) < 1800000) { // 1800000 = 30 mins in milliseconds
+    LiofaWatch[ServerID][UserID].warnings++;
+    LiofaWatch[ServerID][UserID].time = Date.now();    
+
+  } else {
+    LiofaWatch[ServerID][UserID] = {warnings : 1, time : Date.now()};
+
+  }
+  let LiofaUpdate = JSON.stringify(LiofaWatch, null, 2);
+  fs.writeFileSync("./Server Data/WatchList.json", LiofaUpdate);
+  console.log("JSON updated")
+  return LiofaWatch[ServerID][UserID].warnings;
+}
+
+//TODO
+// Add in configurable settings
+// Add commands
+// Add roles that are excluded
+// Learn how to use databases
