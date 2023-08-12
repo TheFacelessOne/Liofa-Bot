ffe5295b3dfc6c2604f45370114e30812ee52a95
TheFacelessOne <wiichris@hotmail.com>
2023-08-12 13:10:38 +0100

    Update start.js
    fixed typo

--- start.js
+++ start.js
@@ -18,9 +18,9 @@ const client = new Client({ intents: myIntents });
 
 // Registers Commands
 client.commands = new Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync('./Commands').filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
+	const command = require(`./Commands/${file}`);
 	// set a new item in the Collection
 	// with the key as the command name and the value as the exported module
 	client.commands.set(command.data.name, command);
